2455eff3356549f09e88facfa3d2a8c8d6fd58a1
Mariam Emad <93050116+Mariam-Emad127@users.noreply.github.com>
2023-12-17 14:46:44 +0200

    Update repository.dart

--- lib/data/repository/repository.dart
+++ lib/data/repository/repository.dart
@@ -1,9 +1,1 @@
-import 'package:advanced_flutter/data/network/failure.dart';
-import 'package:advanced_flutter/data/network/requests.dart';
-import 'package:advanced_flutter/domain/models/models.dart';
-import 'package:dartz/dartz.dart';
-abstract class Repository {
-
-//Either<Failure,Authentication> login(LoginRequest loginRequest) ;
-
-}
+ 
